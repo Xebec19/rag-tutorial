bd13e180494b141de92a7e1d8354b03a5dbdf1d2
xebec19 <rohandeveloper106@gmail.com>
2026-05-13 15:36:10 +0530

    feat: save and retrieve data from vectordb

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 
 
 def main():
+
+    # read file
     pdf_path = f"{Path(__file__).parent}/assets/node-handbook.pdf"
     loader = PyPDFLoader(file_path=pdf_path)
     docs = loader.load()
 
-    print(docs[12])
+    # Split the docs into smaller chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=400)
+
+    chunks = text_splitter.split_documents(documents=docs)
+
+    # Vector embeddings
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+    # Save data in vectordb
+    vector_Store = QdrantVectorStore.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        url="http://localhost:6333",
+        collection_name="learning_rag",
+    )
+
+    print("Indexing of documents done!")
 
 
 if __name__ == "__main__":
